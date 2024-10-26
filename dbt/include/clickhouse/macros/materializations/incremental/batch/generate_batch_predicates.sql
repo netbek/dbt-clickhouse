c0a1c35d07d5f9472d6a68b579b9888d5ed0c955
Hein Bekker <hein@netbek.co.za>
2024-10-26 06:35:38 +0200

    Remove unused macro

--- dbt/include/clickhouse/macros/materializations/incremental/batch/generate_batch_predicates.sql
+++
@@ -1,8 +0,0 @@
-{% macro generate_batch_predicates() %}
-  {%- set materialized = config.get('materialized') -%}
-  {%- set batch_type = config.get('batch_type') -%}
-
-  {% if materialized == 'incremental' and batch_type == 'sequence' %}
-    __BATCH_PREDICATES__
-  {% endif %}
-{% endmacro %}
