d6ac7ab09024181924a740d07316b3da7d49d500
Hein Bekker <hein@netbek.co.za>
2024-10-26 06:34:28 +0200

    Get filter from model config

--- dbt/include/clickhouse/macros/materializations/incremental/batch/sequence/batch_sequence_create_table.sql
+++ dbt/include/clickhouse/macros/materializations/incremental/batch/sequence/batch_sequence_create_table.sql
@@ -1,16 +1,17 @@
 {% macro clickhouse__batch_sequence_create_table(temporary, relation, sql) %}
+  {%- set batch_filter = config.get('batch_filter', '/* __BATCH_FILTER__ */') -%}
   {%- set batch_size = config.get('batch_size', 100000) | int -%}
-  {%- set batch_relation = config.require('batch_relation') -%}
-  {%- set batch_relation_alias = config.get('batch_relation_alias') -%}
+  {%- set batch_table = config.require('batch_table') -%}
   {%- set batch_column = config.require('batch_column') -%}
 
+  {%- set batch_relation = adapter.get_relation(this.database, this.schema, batch_table) -%}
   {%- set min_max = get_min_max(batch_relation, batch_column) | as_native -%}
   {%- set range_min = min_max['min'][0] | int -%}
   {%- set range_max = min_max['max'][0] | int -%}
 
-  {%- if sql.find('__BATCH_PREDICATES__') == -1 -%}
+  {%- if sql.find(batch_filter) == -1 -%}
     {%- set error_message -%}
-      Model '{{ model.unique_id }}' does not include the required string '__BATCH_PREDICATES__' in its sql
+      Model '{{ model.unique_id }}' does not include the required string '{{ batch_filter }}' in its sql
     {%- endset -%}
     {{ exceptions.raise_compiler_error(error_message) }}
   {%- endif -%}
@@ -25,7 +26,7 @@
     {%- set msg = "Loading batch " ~ (offset + 1) ~ " of " ~ (num_batches) -%}
     {{ print(msg) }}
 
-    {%- set filtered_sql = clickhouse__get_batch_sequence_sql(sql, batch_column, batch_size, range_min, range_max, offset, relation_alias=batch_relation_alias) -%}
+    {%- set filtered_sql = clickhouse__get_batch_sequence_sql(sql, batch_filter, batch_column, batch_size, range_min, range_max, offset) -%}
 
     {% call statement('main') %}
       {% if offset == 0 %}
